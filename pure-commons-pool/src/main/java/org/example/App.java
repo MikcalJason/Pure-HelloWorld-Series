7fe232d4b40857eac7336facbdcfb13927404e3b
ZhouQuan <1132155842@qq.com>
2024-07-01 22:38:50 +0800

    init
    代码初始化

--- pure-commons-pool/src/main/java/org/example/App.java
+++ pure-commons-pool/src/main/java/org/example/App.java
@@ -8,14 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-
-        int i = absDivide(10, 3);
-    }
-
-    public static int absDivide(int m, int n) {
-        if (m < n) {
-            return 0;
-        }
-        return absDivide(m - n, n) + 1;
     }
 }
